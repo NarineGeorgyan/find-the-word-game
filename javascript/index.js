4fe2de6023f05b38c526eac6f99ed62c1ae97fbd
Narine Georgyan <narinegeorgyan88@gmail.com>
2022-05-05 14:15:11 +0400

    change in key and  add action in enter button

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,6 @@
 const gameContainer = document.getElementById("game-container");
 const boardContainer = document.querySelector(".board-container");
-const keyArray = ["ա","բ","գ","դ","է","ը","թ","ժ","ի","լ","խ","ծ","կ","del","հ","ձ","ղ","Ճ","մ","յ","ն","շ","ո","չ","enter","պ","ջ","ռ","ս","վ","տ","ր","ց","ի","փ","ք","օ","ֆ"]
+const keyArray = ["ա","բ","գ","դ","ե","զ","է","ը","թ","ժ","ի","լ","խ","ծ","կ","del","հ","ձ","ղ","Ճ","մ","յ","ն","շ","ո","չ","enter","պ","ջ","ռ","ս","վ","տ","ր","ց","ի","փ","ք","օ","ֆ"]
 const keyBoardContainer = document.querySelector(".keyboard-container");
 const boardContainerArray = [];
 let guessArray = [];
@@ -30,12 +30,16 @@ let isWin = false;
     keyCell.innerText = val.toUpperCase();
     keyCell.addEventListener('click',(event)=>{
     if(event.target.id === "DEL"){
-           if (i>0 && max === 5){ 
+           if (i>0 && i===max){ 
                     i--;
                     boardContainer.children[i].innerText=" ";
+                    
+                    console.log(boardContainer.children[i].replaceChild)
+
             }
             }else if(event.target.id === "ENTER"){
                 if(i < max){
+                    
                     document.getElementById("game-over").style.display ="inline-block"
                         document.getElementById("game-over").textContent="Տառերի քանակը 5ից քիչ չի կարող լինել"
                     setInterval(()=>{
@@ -43,12 +47,12 @@ let isWin = false;
                     }, 2000)
                    
                
+                }else if(i===max){
+                    max+=5;
+                    update();
+                    gameOver();
                 }
-                max+=5;
-                update();
-                gameOver();
-                
-                
+            
             }else{
                 if(i<max){
                     boardContainer.children[i].innerText = event.target.id;
@@ -64,25 +68,31 @@ let isWin = false;
        keyBoardContainer.appendChild(keyCell);
     })
 
-function update() {  
+function update() { 
    let counter = 0;
    document.querySelectorAll(".k").forEach((cell,j)=>{
         let letter = cell.innerText;
         let key = document.getElementById(letter);
         if(i<max){
             if(letter === currentWord[j]){
-                cell.classList.add("board-cell-green");
-                cell.classList.remove("k");
-                key.classList.add("board-cell-green");
-                counter ++;
-            }else if(currentWord.includes(letter)){
-                cell.classList.add("board-cell-yellow");
-                cell.classList.remove("k");
                 if(key.classList.contains("board-cell-yellow")){
-                    key.classList.add("board-cell-green");
+                    key.classList.remove("board-cell-yellow")
+                    key.classList.add("board-cell-green")
                 }else{
                     key.classList.add("board-cell-green");
                 }
+                cell.classList.add("board-cell-green");
+                cell.classList.remove("k");
+                counter ++;
+            }else if(currentWord.includes(letter)){
+                           
+                if(key.classList.contains("board-cell-green")){
+                    key.classList.add("board-cell-green");
+                }else{
+                    key.classList.add("board-cell-yellow");
+                }
+                cell.classList.add("board-cell-yellow");
+                cell.classList.remove("k");
                           
             }else{
                 cell.classList.add("board-cell-grey");
